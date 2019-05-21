3a24f2bfd9ff1b57cef49b7402034e0455e5b7da
Vitaliy Honcharenko <vgoncharenko@magento.com>
2019-05-20 20:00:32 -0500

    eee

--- k8s-config/scripts/apply_changes.sh
+++ k8s-config/scripts/apply_changes.sh
@@ -1,5 +1,5 @@
 #!/usr/bin bash
-rm -rf /magento2ce/*
-tar -xzf /magento.zip -C /magento2ce
-rm /magento.zip
-chown -R www-data:www-data /magento2ce
+# rm -rf /magento2ce/*
+# tar -xzf /magento.zip -C /magento2ce
+# rm /magento.zip
+# chown -R www-data:www-data /magento2ce
